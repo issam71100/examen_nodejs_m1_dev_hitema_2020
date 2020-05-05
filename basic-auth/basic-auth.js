6686f303763476d2bb8a9bac0670f4f723e37284
Issam Kadar <issam.kadar@gmail.com>
2020-05-05 14:44:34 +0200

    BASIC-AUTH

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -2,8 +2,25 @@ const crypto = require('crypto');
 
 function sha1Encode(data) {
     // To be implemented!
+    const sha1Encode = crypto.createHash('sha1');
+    sha1Encode.update(data);
+    return sha1Encode.digest('hex');
 }
 
 module.exports.digestAuth = (request, response, next) => {
     // To be implemented!
+
+    const authorization = request.headers.authorization;  // 'Basic xxxx'
+    const encoded = authorization.replace('Basic ', '');
+
+    const decoded = Buffer.from(encoded, 'base64').toString('utf8');
+
+    const authentication = decoded.split(':');
+
+    const isValid = authentication[0] === 'node'
+        && authentication[1] ===  sha1Encode('password');
+    
+    // si pas authentifié
+    const HTTP_CODE_UNAUTHORIZED = 401; 
+    isValid ? next() : response.sendStatus(HTTP_CODE_UNAUTHORIZED);
 }
